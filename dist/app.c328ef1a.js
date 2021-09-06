cdcfb879d516eee54520948973a93a265d8ae6f1
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-09-06 23:37:38 +0000

    conditionals

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -121,7 +121,9 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 new Vue({
   el: "#vue-app",
   data: {
-    name: 'ramos'
+    name: 'Conditionals',
+    error: false,
+    success: false
   }
 });
 },{}],"../../../Users/Ramos/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -152,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53629" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60433" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
