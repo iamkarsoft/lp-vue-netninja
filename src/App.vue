d6153be650c5141ae174aca357576fdf07fcd9a1
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-09-13 00:23:35 +0000

    filters

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
-      <Header />
-      <Footer />
-  </div>
+    <div id="app">
+        <Header />
+        <h2>{{title | to-uppercase}}</h2>
+        <p>{{content | snippet}}</p>
+        <Footer />
+    </div>
 </template>
-
 <script>
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Header,
-    Footer
-  }
+    name: 'App',
+    components: {
+        Header,
+        Footer
+    },
+    data() {
+        return {
+            title: "Lorem, ipsum dolor sit amet consectetur.",
+            content: "Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Esse nisi laborum odit facilis blanditiis suscipit reprehenderit, at pariatur illo, consequuntur!"
+        }
+    }
 
 };
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.filter('to-uppercase',function(value){
+  return value.toUpperCase();
+})
+
+Vue.filter('snippet',function(value){
+  return value.slice(0,100)+'...'
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
